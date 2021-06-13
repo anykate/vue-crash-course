fbecef13e2272437ab0282850d4cb5076f6a447b
Aniket Aryamane <aryamane.aniket@gmail.com>
2021-06-13 23:13:12 +0530

    Introducing PrimeVue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,38 +1,30 @@
 <template>
   <div>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div class="container-fluid">
-        <router-link class="navbar-brand mb-0 h1" to="/">Navbar</router-link>
-
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navmenu"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navmenu">
-          <ul class="navbar-nav ms-auto">
-            <li class="nav-item">
-              <router-link :to="{ name: 'Home' }" class="nav-link">
-                Home
-              </router-link>
-            </li>
-            <li class="nav-item">
-              <router-link :to="{ name: 'About' }" class="nav-link">
-                About
-              </router-link>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+    <TabMenu :model="items" />
   </div>
 </template>
 
 <script>
+import { ref } from 'vue'
 export default {
   name: "Navbar",
+  setup() {
+    const items = ref([
+      {
+        label: 'Home',
+        icon: 'pi pi-fw pi-home',
+        to: '/'
+      },
+      {
+        label: 'About',
+        icon: 'pi pi-fw pi-calendar',
+        to: '/about'
+      },
+    ])
+
+    return {
+      items
+    }
+  }
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,19 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-createApp(App).use(router).mount('#app')
+import PrimeVue from "primevue/config";
+import TabMenu from "primevue/tabmenu";
+
+import "primevue/resources/primevue.min.css";
+import "primeflex/primeflex.min.css";
+import "primeicons/primeicons.css";
+import "primevue/resources/themes/saga-purple/theme.css";
+
+const app = createApp(App);
+
+app.use(PrimeVue, { ripple: true });
+app.component("TabMenu", TabMenu);
+app.use(router);
+
+app.mount("#app");
